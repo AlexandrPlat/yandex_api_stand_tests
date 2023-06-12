22699da5538943440d6e2616ed2a41befa0ec7df
Alexandr Platnov <mikkypl88@yandex.ru>
2023-06-12 19:41:59 +0300

    My first commit Platnov

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://9d1f3cd3-bddc-48cb-88bd-09d4684a54ca.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
